299b4cda34334ee016fec9c6b230dded7bd92765
raineio <64136899+raineio@users.noreply.github.com>
2022-04-23 02:04:42 +0200

    fix registering and unregistering

--- src/CustomLeaderboardManager.cpp
+++ src/CustomLeaderboardManager.cpp
@@ -1,20 +1,23 @@
 #include "shared/CustomLeaderboardManager.hpp"
 #include "shared/Models/CustomLeaderboard.hpp"
 #include <vector>
+#include <find>
 
 namespace LeaderboardCore {
     std::vector<INotifyLeaderboardChange*> CustomLeaderboardManager::m_notifyCustomLeaderboardChanges;
     std::vector<CustomLeaderboard*> CustomLeaderboardManager::m_customLeaderboards;
 
     CustomLeaderboardManager::Register(CustomLeaderboard* customLeaderboard) {
-        if (!customLeaderboards->Contains(m_customLeaderboards)) {
-            m_customLeaderboards.push_back(customLeaderboard);
+        auto itr = std::find(m_customLeaderboards.begin(), m_customLeaderboards.end(), customLeaderboard);
+        if (itr == m_customLeaderboards.end()) {
+            m_customLeaderboards.emplace_back(customLeaderboard);
             OnLeaderboardChange();
         }
     }
 
     CustomLeaderboardManager::Unregister(CustomLeaderboard* customLeaderboard) {
-        m_customLeaderboards.pop_back(customLeaderboard);
+        auto itr = std::find(m_customLeaderboards.begin(), m_customLeaderboards.end(), customLeaderboard);
+        m_customLeaderboards.erase(itr, itr++);
         OnLeaderboardChange();
     }
 
