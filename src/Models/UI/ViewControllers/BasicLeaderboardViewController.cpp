42bb610772cc23810fd266541add754f21fc66d8
raineio <64136899+raineio@users.noreply.github.com>
2022-05-07 07:17:02 +0200

    Get basic stuff up on DoubleTextPanel and started on BasicLeaderboard

--- src/Models/UI/ViewControllers/BasicLeaderboardViewController.cpp
+++ src/Models/UI/ViewControllers/BasicLeaderboardViewController.cpp
@@ -1,10 +1,31 @@
 #include "Models/UI/ViewControllers/BasicLeaderboardViewController.hpp"
+#include "questui/shared/BeatSaberUI.hpp"
+#include "questui/shared/CustomTypes/Components/Backgroundable.hpp"
+#include "UnityEngine/UI/VerticalLayoutGroup.hpp"
+#include "UnityEngine/UI/LayoutElement.hpp"
+#include "UnityEngine/TextAlignment.hpp"
+#include "TMPro/TextMeshPro.hpp"
+
 
 DEFINE_TYPE(LeaderboardCore::Models::UI::ViewControllers, BasicLeaderboardViewController)
 
 namespace LeaderboardCore::Models::UI::ViewControllers {
     void BasicLeaderboardViewController::DidActivate(bool firstActivation, bool addedToHeirarchy, bool screenSystemDisabling) {
+        if (firstActivation) {
+            auto horizontal = QuestUI::BeatSaberUI::CreateHorizontalLayoutGroup(this->get_transform());
+            
+            horizontal->get_gameObject()->GetComponent<UnityEngine::UI::LayoutElement*>()->set_preferredHeight(70);
+            horizontal->get_gameObject()->GetComponent<UnityEngine::UI::LayoutElement*>()->set_preferredWidth(100);
 
+            auto segmentedIconVertical = QuestUI::BeatSaberUI::CreateVerticalLayoutGroup(horizontal->get_transform());
+
+            segmentedIconVertical->get_gameObject()->GetComponent<UnityEngine::UI::LayoutElement*>()->set_preferredHeight(20);
+            segmentedIconVertical->get_gameObject()->GetComponent<UnityEngine::UI::LayoutElement*>()->set_preferredWidth(9);
+
+            auto leaderboard = QuestUI::BeatSaberUI::CreateVerticalLayoutGroup(horizontal->get_transform());
+
+            leaderboard->get_gameObject()->GetComponent<UnityEngine::UI::LayoutElement*>();
+        }
     }
 
     void BasicLeaderboardViewController::Awake() {
--- src/Models/UI/ViewControllers/BasicPanelViewController.cpp
+++ src/Models/UI/ViewControllers/BasicPanelViewController.cpp
@@ -1,9 +1,12 @@
 #include "Models/UI/ViewControllers/BasicPanelViewController.hpp"
+#include "questui/shared/BeatSaberUI.hpp"
 
 DEFINE_TYPE(LeaderboardCore::Models::UI::ViewControllers, BasicPanelViewController)
 
 namespace LeaderboardCore::Models::UI::ViewControllers {
     void BasicPanelViewController::DidActivate(bool firstActivation, bool addedToHeirarchy, bool screenSystemDisabling) {
-
+        if (firstActivation) {
+            
+        }
     }
 }
