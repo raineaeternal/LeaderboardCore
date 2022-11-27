7ff2bc993baffa5596ad90f4ba0bb5ab6e458a7e
Raine <64136899+raineio@users.noreply.github.com>
2022-11-27 03:18:25 +0100

    implement Register, Unregister and OnLeaderboardsChanged();

--- src/Managers/CustomLeaderboardManager.cpp
+++ src/Managers/CustomLeaderboardManager.cpp
@@ -1,18 +1,30 @@
 #include "Managers/CustomLeaderboardManager.hpp"
+#include "Models/CustomLeaderboard.hpp"
 
 namespace LeaderboardCore::Managers {
     std::unordered_set<Interfaces::INotifyLeaderboardChange*> CustomLeaderboardManager::notifyCustomLeaderboardsChanges;
     std::unordered_map<std::string, Models::CustomLeaderboard*> CustomLeaderboardManager::customLeaderboardsById;
 
     void CustomLeaderboardManager::Register(Models::CustomLeaderboard* customLeaderboard, const ModInfo& modInfo) {
-
+        if (!customLeaderboardsById.contains(customLeaderboard->get_LeaderboardId())) {
+            customLeaderboardsById[customLeaderboard->get_LeaderboardId()] = customLeaderboard;
+            OnLeaderboardsChanged();
+        }
     }
 
     void CustomLeaderboardManager::Unregister(Models::CustomLeaderboard* customLeaderboard) {
-        
+        customLeaderboardsById.erase(customLeaderboard->get_LeaderboardId());
+        OnLeaderboardsChanged();
     }
 
     void CustomLeaderboardManager::OnLeaderboardsChanged() {
-        
+        std::set<LeaderboardCore::Models::CustomLeaderboard *> values;
+        for (const auto& [key, value] : customLeaderboardsById) {
+            values.insert(value);
+        }
+
+        for (const auto& lb : notifyCustomLeaderboardsChanges) {
+            lb->OnLeaderboardsChanged(values, customLeaderboardsById);
+        }
     }
 }
