d85ea8324194f8b07b60060c00a88eb803941515
Raine <64136899+raineio@users.noreply.github.com>
2022-11-27 03:18:00 +0100

    make OnLeaderboardsChanged(); actually usable

--- shared/Interfaces/INotifyLeaderboardChange.hpp
+++ shared/Interfaces/INotifyLeaderboardChange.hpp
@@ -2,6 +2,7 @@
 
 namespace LeaderboardCore::Interfaces {
     class INotifyLeaderboardChange {
+    public:
         virtual void OnLeaderboardsChanged(const std::set<Models::CustomLeaderboard*>& orderedCustomLeaderboards, const std::unordered_map<std::string, Models::CustomLeaderboard*>& customLeaderboardsById) = 0;
     };
 }
