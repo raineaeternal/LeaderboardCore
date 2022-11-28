5c83827132dbb57b2f0bbd92d14c8b19add020df
Raine <64136899+raineio@users.noreply.github.com>
2022-11-28 23:01:12 +0100

    Port functionality of ScoreSabers (PC) CellForIdx patch

---
+++ src/Hooks/LeaderboardTableView_CellForIdx.cpp
@@ -0,0 +1,26 @@
+#include "hooking.h"
+
+#include "GlobalNamespace/LeaderboardTableView.hpp"
+#include "GlobalNamespace/LeaderboardTableCell.hpp"
+#include "HMUI/TableView.hpp"
+#include "HMUI/TableCell.hpp"
+
+#include "TMPro/TextMeshProUGUI.hpp"
+
+MAKE_AUTO_HOOK_MATCH(LeaderboardTableView_CellForIdx, &GlobalNamespace::LeaderboardTableView::CellForIdx, HMUI::TableCell*, GlobalNamespace::LeaderboardTableView* self, HMUI::TableView* tableView, int idx) {
+    LeaderboardTableView_CellForIdx(self, tableView, idx);
+
+    // REMOVEME: temp value.
+    bool isOST = false;
+
+    if (self->get_transform()->get_parent()->get_parent()->get_name() == "PlatformLeaderboardViewController") {
+        GlobalNamespace::LeaderboardTableCell* tableCell = reinterpret_cast<GlobalNamespace::LeaderboardTableCell*>(tableView);
+        TMPro::TextMeshProUGUI* playerNameText = tableCell->playerNameText;
+
+        if (isOST) {
+            playerNameText->set_richText(false);
+        } else {
+            playerNameText->set_richText(true);
+        }
+    }
+}
