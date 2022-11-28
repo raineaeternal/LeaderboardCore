07ee22a0a0be777f399ce024d753aceee571354a
RedBrumbler <redbrumbler.official+github@gmail.com>
2022-11-28 20:26:53 +0100

    Implement scoring storage

--- src/LeaderboardCore.cpp
+++ src/LeaderboardCore.cpp
@@ -1,4 +1,5 @@
 #include "LeaderboardCore.hpp"
+#include "ScoreDisabling.hpp"
 #include "Managers/CustomLeaderboardManager.hpp"
 
 namespace LeaderboardCore {
@@ -9,6 +10,36 @@ namespace LeaderboardCore {
 
         void UnregisterLeaderboard(Models::CustomLeaderboard* leaderboard) {
             Managers::CustomLeaderboardManager::Unregister(leaderboard);
+        }
+    }
+
+    namespace Scoring {
+        void Disable(const ModInfo& modInfo, const std::string_view& reason) {
+            ScoreDisabling::Disable(modInfo, reason);
+        }
+
+        void Disable(const std::string& leaderboardId, const ModInfo& modInfo, const std::string_view& reason) {
+            ScoreDisabling::Disable(leaderboardId, modInfo, reason);
+        }
+
+        void Enable(const ModInfo& modInfo) {
+            ScoreDisabling::Enable(modInfo);
+        }
+
+        void Enable(const std::string& leaderboardId, const ModInfo& modInfo) {
+            ScoreDisabling::Enable(leaderboardId, modInfo);
+        }
+
+        bool GetScoreSubmissionAllowed(const std::string& leaderboardId) {
+            return ScoreDisabling::GetScoreSubmissionAllowed(leaderboardId);
+        }
+
+        const std::unordered_map<ModInfo, std::string>& GetDisablingModInfos(const std::string& leaderboardId) {
+            return ScoreDisabling::GetDisablingModInfos(leaderboardId);
+        }
+
+        std::vector<std::string> GetLeaderboardIds() {
+            return ScoreDisabling::GetLeaderboardIds();
         }
     }
 }
