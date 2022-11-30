9602eab3f503088a4ee9db709a26f1a9e7528c2c
RedBrumbler <redbrumbler.official+github@gmail.com>
2022-11-30 23:44:16 +0100

    Actually use the return value of the orig call

--- src/Hooks/LeaderboardTableView_CellForIdx.cpp
+++ src/Hooks/LeaderboardTableView_CellForIdx.cpp
@@ -8,13 +8,12 @@
 #include "TMPro/TextMeshProUGUI.hpp"
 
 MAKE_AUTO_HOOK_MATCH(LeaderboardTableView_CellForIdx, &GlobalNamespace::LeaderboardTableView::CellForIdx, HMUI::TableCell*, GlobalNamespace::LeaderboardTableView* self, HMUI::TableView* tableView, int idx) {
-    LeaderboardTableView_CellForIdx(self, tableView, idx);
-
+    auto tableCell = LeaderboardTableView_CellForIdx(self, tableView, idx);
     // REMOVEME: temp value.
     bool isOST = false;
 
     if (self->get_transform()->get_parent()->get_parent()->get_name() == "PlatformLeaderboardViewController") {
-        GlobalNamespace::LeaderboardTableCell* tableCell = reinterpret_cast<GlobalNamespace::LeaderboardTableCell*>(tableView);
+        GlobalNamespace::LeaderboardTableCell* tableCell = reinterpret_cast<GlobalNamespace::LeaderboardTableCell*>(tableCell);
         TMPro::TextMeshProUGUI* playerNameText = tableCell->playerNameText;
 
         if (isOST) {
@@ -23,4 +22,5 @@ MAKE_AUTO_HOOK_MATCH(LeaderboardTableView_CellForIdx, &GlobalNamespace::Leaderbo
             playerNameText->set_richText(true);
         }
     }
+    return tableCell;
 }
